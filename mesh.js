22e47682f570df486e18198d1c295f05f2df8ca5
Ben Patterson <bmizepatterson@gmail.com>
2018-07-25 16:53:49 -0400

    Add basic cell-drawing functionality

--- mesh.js
+++ mesh.js
@@ -1,4 +1,6 @@
-var Cells;
+var Cells = [];
+var workspace = document.getElementById("workspace");
+var debug = document.getElementById("debugspace");
 
 function Cell(x, y, r) {
 	this.x = x;
@@ -6,27 +8,49 @@ function Cell(x, y, r) {
 	this.r = r;
 }
 
-function addCell() {
-	// Find a new spot
-	var newCell = new Cell(100, 200, 20);
+function addCell(event) {
+	var newCell = new Cell(event.clientX - workspace.offsetLeft, event.clientY - workspace.offsetTop, 20);
 	newCell.id = Cells.length;
 	// Add it to the list of Cells
-	Cells[] = newCell;
+	Cells.push(newCell);
 	// Draw it
 	drawCell(newCell);
-	document.getElementById("debugspace").innerHTML = Cells;
+	print_cell(newCell);
 }
 
-
 function drawCell(Cell) {
-
 	// Get canvas and drawing object
-	var canvas = document.getElementById("workspace");
-	var ctx = canvas.getContext("2d");
-	
+	var ctx = workspace.getContext("2d");
 	// Draw a circle to represent a neuron
 	ctx.beginPath();
 	ctx.arc(Cell.x,Cell.y,Cell.r,0,2*Math.PI);
 	ctx.stroke();
+}
 
+function clearWorkspace() {
+	// Clear out Cells array
+	Cells = [];
+	// Erase workspace
+	var ctx = workspace.getContext("2d");
+	ctx.clearRect(0,0,500,500);
+	// Clear debug area
+	debug.innerHTML = '';
+
+}
+
+// DEBUG FUNCTIONS
+
+function print_cell(cell) {
+	debug.innerHTML += "<p>Cell " + cell.id + ": (" + cell.x + ", " + cell.y + ")</p>";
+}
+
+function showCoords(event) {
+    var x = event.clientX - workspace.offsetLeft;
+    var y = event.clientY - workspace.offsetTop;
+    var coor = "(" + x + ", " + y + ")";
+    document.getElementById("mousecoords").innerHTML = coor;
+}
+
+function clearCoor() {
+    document.getElementById("mousecoords").innerHTML = "";
 }
