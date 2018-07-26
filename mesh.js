9081378806c2db19a6d72058312e85ee3e493c8b
Ben Patterson <bmizepatterson@gmail.com>
2018-07-25 22:50:53 -0400

    Add stimulate functionality

--- mesh.js
+++ mesh.js
@@ -1,12 +1,17 @@
 var Cells = [];
+var Dendrites = [];
 var workspace = document.getElementById("workspace");
 var debug = document.getElementById("debugspace");
 var addSelector = 'cell';
 var radius = 20;
+var highlightedCell = -1;
+var textAlign = 'center';
+var font = '12px sans-serif';
 
 // Utility functions
 function distance(x1, y1, x2, y2) {
-	return Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+	// Use the distance formula to calculate the distance between two points.
+	return Math.ceil(Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2)));
 }
 
 // Object constructors
@@ -18,30 +23,105 @@ function Cell(x, y, r) {
 	this.potential = 0;
 	this.inputCells = [];
 	this.outputCells = [];
+	this.highlighted = true;
+	this.selected = false;
+	this.lineWidth = 4;
+	this.outerRadius = r + this.lineWidth;
 }
 
-function workspaceClick(event) {
-	addMeshElement(event);
+function Dendrite(originCell = null, destinationCell) {
+	this.originCell = originCell;
+	this.destinationCell = destinationCell; 
+}
+
+function workspaceSetup() {
+	// Initial setup of the workspace includes one cell and one dendrite
+	var firstCell = new Cell(75, 250, radius);
+	firstCell.id = 0;
+	firstCell.highlighted = false;
+	firstCell.lineWidth = 1;
+	Cells.push(firstCell);
+
+	var firstDendrite = new Dendrite(null, 0);
+	firstDendrite.id = 0;
+	Dendrites.push(firstDendrite);
+	var ctx = workspace.getContext("2d");
+	ctx.beginPath();
+	ctx.moveTo(0, 250);
+    ctx.lineTo(55, 250);
+    ctx.arc(firstCell.x,firstCell.y,firstCell.r,Math.PI,4*Math.PI);
+    ctx.stroke();
+
+    ctx.textAlign = textAlign;
+    ctx.font = font;
+    ctx.fillText(firstCell.potential, 75, 255);
+
 	printMeshStateTable();
 }
 
-function addMeshElement(event) {
+function workspaceClick(event) {
 	if (addSelector === 'cell') {
-		addCell(event);
+		var newCellX = event.clientX - workspace.offsetLeft + window.pageXOffset;
+		var newCellY = event.clientY - workspace.offsetTop + window.pageYOffset;
+		addCell(newCellX, newCellY);
+	} else if (addSelector === 'dendrite') {
+		addDendrite(event);
+	}
+	printMeshStateTable();
+}
+
+function workspaceMove(event) {
+	// Show the mouse coordinates within the canvas for reference
+	var x = event.clientX - workspace.offsetLeft + window.pageXOffset;
+    var y = event.clientY - workspace.offsetTop + window.pageYOffset;
+    document.getElementById("mousecoords").innerHTML = "(" + x + ", " + y + ")";
+    document.getElementById("collision").innerHTML = 'highlighted cell: ' + highlightedCell;
+
+	// Check for collision with a cell body. 
+	// Ignore collisions with the first Cell
+	var collision = false;
+	for (var i = 0; i < Cells.length; i++) {
+		if (distance(x, y, Cells[i].x, Cells[i].y) <= Cells[i].r) {
+			// If the distance between the mouse and this cell's center is less than this cell's radius, then we have a collision.
+			collision = true;
+			eraseCell(Cells[i]);
+			// Set selected flag
+			highlightedCell = i;
+			Cells[i].highlighted = true;
+			Cells[i].lineWidth = 4;
+			// Redraw cell with colored border
+			drawCell(Cells[i], '#3f51b5');
+			break;
+		}
+	}
+
+	if (collision == false) {
+		highlightedCell = -1;
+		removeHighlights();
 	}
 }
 
-function addCell(event) {
-	// Create a new cell object
-	var newCellX = event.clientX - workspace.offsetLeft + window.pageXOffset;
-	var newCellY = event.clientY - workspace.offsetTop + window.pageYOffset;
+function drawDendrite(Dendrite) {
+	// Get canvas and drawing object
+	var ctx = workspace.getContext("2d");
+
+	// Determine starting position
+
+	// Determine ending position
+
+	// Draw a line to represent a dendrite
+
+}
+
+function addCell(newCellX, newCellY) {
+	// Create a new cell object	
 	var newCell = new Cell(newCellX, newCellY, radius);
 
 	// Check for collisions
 	var collision = false;
 	for (var i = 0; i < Cells.length; i++) {
 		// Use the distance formula. If the distance between the two center points is less than the sum of the two radii, then a collision has occurred.
-		if (distance(newCellX, newCellY, Cells[i].x, Cells[i].y) < (Cells[i].r + newCell.r)) {
+		if (distance(newCellX, newCellY, Cells[i].x, Cells[i].y) < (Cells[i].r + newCell.r + 3)) {
 			collision = true;
 			break;
 		}
@@ -51,27 +131,64 @@ function addCell(event) {
 	if (collision == false) {		
 		newCell.id = Cells.length;
 		Cells.push(newCell);
-		drawCell(newCell);
+		drawCell(newCell, '#3f51b5');
+		return newCell;
+	} else {
+		return false;
 	}
 }
 
-function drawCell(Cell) {
+function drawCell(Cell, color, fill = false) {
 	// Get canvas and drawing object
 	var ctx = workspace.getContext("2d");
+
+	ctx.strokeStyle = color;
 	// Draw a circle to represent a neuron
+	ctx.lineWidth = Cell.lineWidth;
+	ctx.beginPath();
+	ctx.arc(Cell.x,Cell.y,Cell.r,0,2*Math.PI);
+	ctx.stroke();
+	if (fill == true) {
+		ctx.fillStyle = color;
+		ctx.fill();
+	}
+}
+
+function eraseCell(Cell) {
+	// Draw a white circle overtop of the current Cell, effectively erasing it
+	// This does not delete the cell! Used for redrawing it (i.e. when it is selected).
+	var ctx = workspace.getContext("2d");
+	ctx.strokeStyle = '#fff';
+	ctx.lineWidth = Cell.lineWidth+2;
 	ctx.beginPath();
 	ctx.arc(Cell.x,Cell.y,Cell.r,0,2*Math.PI);
 	ctx.stroke();
 }
 
+function removeHighlights() {
+	// Check that no objects are selected
+	for (var i = 0; i < Cells.length; i++) {
+		if (Cells[i].highlighted == true) {
+			eraseCell(Cells[i]);
+			// Set selected flag
+			Cells[i].highlighted = false;
+			Cells[i].lineWidth = 1;
+			// Redraw the cell without the colored border
+			drawCell(Cells[i], '#000');
+		}
+	}
+}
+
 function clearWorkspace() {
 	// Clear out Cells array
 	Cells = [];
+	Dendrites = [];
 	// Erase workspace
 	var ctx = workspace.getContext("2d");
 	ctx.clearRect(0,0,500,500);
 	// Clear debug area
 	debug.innerHTML = '';
+	workspaceSetup();
 }
 
 function selectAddCell() {
@@ -83,52 +200,60 @@ function selectAddCell() {
 	}
 	document.getElementById("tip").innerHTML = 'Click anywhere to add a cell body.';
 	// Deselect the other ADD buttons
-	if (document.getElementById("selectAddInput").classList.contains('w3-red')) {
-		document.getElementById("selectAddInput").classList.remove('w3-red');
-		document.getElementById("selectAddInput").classList.add('w3-black');
+	if (document.getElementById("selectAddDendrite").classList.contains('w3-red')) {
+		document.getElementById("selectAddDendrite").classList.remove('w3-red');
+		document.getElementById("selectAddDendrite").classList.add('w3-black');
 	}
-	if (document.getElementById("selectAddOutput").classList.contains('w3-red')) {
-		document.getElementById("selectAddOutput").classList.remove('w3-red');
-		document.getElementById("selectAddOutput").classList.add('w3-black');
-	}
+
 }
 
-function selectAddInput() {
+function selectAddDendrite() {
 	// Set current selection setting
-	addSelector = 'input';
-	if (document.getElementById("selectAddInput").classList.contains('w3-black')) {
-		document.getElementById("selectAddInput").classList.remove('w3-black');
-		document.getElementById("selectAddInput").classList.add('w3-red');
+	addSelector = 'dendrite';
+	if (document.getElementById("selectAddDendrite").classList.contains('w3-black')) {
+		document.getElementById("selectAddDendrite").classList.remove('w3-black');
+		document.getElementById("selectAddDendrite").classList.add('w3-red');
 	}
-	document.getElementById("tip").innerHTML = "Click and drag to add an input dendrite.";
+	document.getElementById("tip").innerHTML = 'To add a dendrite, click and drag from the source cell to the destination cell.';
 	// Deselect the other ADD buttons
 	if (document.getElementById("selectAddCell").classList.contains('w3-red')) {
 		document.getElementById("selectAddCell").classList.remove('w3-red');
 		document.getElementById("selectAddCell").classList.add('w3-black');
 	}
-	if (document.getElementById("selectAddOutput").classList.contains('w3-red')) {
-		document.getElementById("selectAddOutput").classList.remove('w3-red');
-		document.getElementById("selectAddOutput").classList.add('w3-black');
-	}
+
 }
 
-function selectAddOutput() {
-	// Set current selection setting
-	addSelector = 'output';
-	if (document.getElementById("selectAddOutput").classList.contains('w3-black')) {
-		document.getElementById("selectAddOutput").classList.remove('w3-black');
-		document.getElementById("selectAddOutput").classList.add('w3-red');
+function stimulate(Cell, power) {
+	if (Cell.potential + power >= Cell.threshold) {
+		fire(Cell);
+		// Recursively stimulate all output cells
+		if (Cell.outputCells.length > 0) {
+			for (var i = 0; i < Cell.outputCells.length; i++) {
+				stimulate(Cells[i], power);
+			}
+		}
+		Cell.potential = 0;
+	} else {
+		Cell.potential = Cell.potential + power;
 	}
-	document.getElementById("tip").innerHTML = 'Click and drag to add an output dendrite.';
-	// Deselect the other ADD buttons
-	if (document.getElementById("selectAddCell").classList.contains('w3-red')) {
-		document.getElementById("selectAddCell").classList.remove('w3-red');
-		document.getElementById("selectAddCell").classList.add('w3-black');
-	}
-	if (document.getElementById("selectAddInput").classList.contains('w3-red')) {
-		document.getElementById("selectAddInput").classList.remove('w3-red');
-		document.getElementById("selectAddInput").classList.add('w3-black');
-	}
+	updatePotential(Cell);
+	printMeshStateTable();
+}
+
+function updatePotential(Cell) {
+	var ctx = workspace.getContext('2d');
+	// Erase old potential
+	ctx.clearRect(Cell.x-11, Cell.y-10, 22, 20);
+
+	// Draw new potential
+	ctx.textAlign = textAlign;
+    ctx.font = font;
+    ctx.fillText(Cell.potential, Cell.x, Cell.y+5);
+}
+
+function fire(Cell) {
+	// Animate a cell firing
+
 }
 
 // DEBUG FUNCTIONS
@@ -147,13 +272,6 @@ function printMeshStateTable() {
 	}
 	html += '</table>';
 	debug.innerHTML = html;
-}
-
-function showCoords(event) {
-    var x = event.clientX - workspace.offsetLeft;
-    var y = event.clientY - workspace.offsetTop;
-    var coor = "(" + x + ", " + y + ")  Scrolltop: " + window.pageYOffset;
-    document.getElementById("mousecoords").innerHTML = coor;
 }
 
 function clearCoor() {
